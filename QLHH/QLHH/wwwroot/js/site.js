040d35ffe2485d7c7568a38e07ebbd38dd1e3ea3
DESKTOP-LU0C24T\cuong <bacuongtr@gmail.com>
2017-06-10 12:49:39 +0700

    add libs

--- QLHH/QLHH/wwwroot/js/site.js
+++ QLHH/QLHH/wwwroot/js/site.js
@@ -3,12 +3,8 @@ var helper = {};
 
 //statis value
 (function (helper) {
-    helper.MessageType = {
-        Success: 'alert alert-success',
-        Info: 'alert alert-info',
-        Warning: 'alert alert-warning',
-        Danger: 'alert alert-danger'
-    }
+   
+
 })(helper || {});
 
 (function (helper) {
@@ -24,9 +20,9 @@ var helper = {};
             if (data.submitUrl) {
                 $.post(data.submitUrl, { key: data.pk, value: currentValue }, function (result) {
                     if (result.results === true) {
-                        helper.showAlertMessageBox("Success", "Thành công");
+                        $.notify({message: 'Thành công'}, {type: 'success'});
                     } else {
-                        helper.showAlertMessageBox("Danger", "Lỗi");
+                        $.notify({ message: 'Lỗi' }, { type: 'danger' });
                     }
                 });
             }
@@ -37,14 +33,6 @@ var helper = {};
 })(helper || {});
 
 
-(function (helper) {
 
-    helper.showAlertMessageBox = function (messageType, content) {
-        $('#messages').removeClass('hide').addClass(`${helper.MessageType[messageType]} alert-dismissible`).slideDown().show();
-        $('#messages_content').html(`<h4>${content}</h4>`);
-        $('#modal').modal('show');
-    }
-})(helper || {});
-
-//init function
+//init select2 function
 helper.InitSelect2();
